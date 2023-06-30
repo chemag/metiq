e0fad639d41595694317bc3b078b5129564fed9f
Chema Gonzalez <chemag@gmail.com>
2023-06-30 05:32:34 -0700

    metiq: fix length of the target needle

--- src/audio_analyze.py
+++ src/audio_analyze.py
@@ -104,10 +104,10 @@ def audio_analyze_wav(infile, **kwargs):
         # There is a bug (https://github.com/scipy/scipy/issues/15620)
         # resulting in all zeroes unless inout is cast to float
         inaud = scipy.signal.resample_poly(inaud.astype(np.float32), int(samplerate/100), int(haystack_samplerate/100), padtype='mean')
-    # generate a 1-period needle
-    needle = audio_common.generate_beep(beep_period_sec, **kwargs)[0:audio_common.DEFAULT_BEEP_DURATION_SAMPLES]
+    # generate a single needle (without the silence)
+    needle_target = audio_common.generate_beep(beep_period_sec, **kwargs)[0:kwargs["beep_duration_samples"]]
     # calculate the correlation signal
-    index_list, correlation = get_correlation_indices(inaud, needle)
+    index_list, correlation = get_correlation_indices(inaud, needle_target)
     # add a samplerate-based timestamp
     audio_results = [
         (index, index / samplerate, correlation[index]) for index in index_list
