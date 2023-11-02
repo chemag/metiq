99d00ea50b4b0acf0467f8886d95d85653b1beb0
JohanBlome <jblome@fb.com>
2023-11-02 13:59:10 -0700

    metiq: add latency detection and logging
    From atools.
    
    Includes:
    * changed default to use chirpinstead of beeps (for more accurate detections)
    * added a csv file containing
        - audio latency
        - video latency
        - a/v synch
      where it makes sense.
    
    Signed-off-by: JohanBlome <jblome@fb.com>

--- src/audio_generate.py
+++ src/audio_generate.py
@@ -9,5 +9,5 @@ import audio_common
 
 
 def audio_generate(duration_sec, output_filename, **kwargs):
-    aud = audio_common.generate_beep(duration_sec, **kwargs)
+    aud = audio_common.generate_chirp(duration_sec, **kwargs)
     scipy.io.wavfile.write(output_filename, audio_common.DEFAULT_SAMPLERATE, aud)
