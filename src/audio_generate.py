117c79a1f869735b8c9cc9dda52616a008290fba
JohanBlome <jblome@fb.com>
2024-01-03 16:05:47 -0800

    metiq: add audio file generator
    Make it possible to use an arbitrary audio file as signal.
    The name of the signal will be printed in the video.
    
    Signed-off-by: JohanBlome <jblome@fb.com>

--- src/audio_generate.py
+++ src/audio_generate.py
@@ -9,5 +9,11 @@ import audio_common
 
 
 def audio_generate(duration_sec, output_filename, **kwargs):
-    aud = audio_common.generate_chirp(duration_sec, **kwargs)
+    """Generate audio file with given duration and parameters."""
+    audio_sample = kwargs.get("audio_sample", "")
+    aud = None
+    if len(audio_sample) > 0:
+        aud = audio_common.generate_sample_based(duration_sec, **kwargs)
+    else:
+        aud = audio_common.generate_chirp(duration_sec, **kwargs)
     scipy.io.wavfile.write(output_filename, audio_common.DEFAULT_SAMPLERATE, aud)
