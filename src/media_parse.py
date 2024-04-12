175fa25be2bad2fd6a2656751c4b2b7c2de8a6ac
Chema Gonzalez <chemag@meta.com>
2024-04-12 11:19:51 -0700

    metiq: move media parsing to separate file

---
+++ src/media_parse.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+
+"""media_parse.py module description."""
+
+
+import audio_parse
+import video_parse
+
+
+def media_parse_coverage_video(
+    infile,
+    **kwarg,
+):
+    return video_parse.calc_coverage(
+        infile,
+        width=kwarg.get("width", 0),
+        height=kwarg.get("height", 0),
+        pixel_format=kwarg.get("pixel_format"),
+        debug=kwarg.get("debug", False),
+    )
+
+
+def media_parse_audio(
+    pre_samples,
+    samplerate,
+    beep_freq,
+    beep_duration_samples,
+    beep_period_sec,
+    scale,
+    infile,
+    outfile,
+    debug,
+    **kwargs,
+):
+    min_separation_msec = kwargs.get("min_separation_msec", 50)
+    min_match_threshold = kwargs.get("min_match_threshold", 10)
+    audio_sample = kwargs.get("audio_sample", "")
+
+    audio_results = audio_parse.audio_parse(
+        infile,
+        pre_samples=pre_samples,
+        samplerate=samplerate,
+        beep_freq=beep_freq,
+        beep_duration_samples=beep_duration_samples,
+        beep_period_sec=beep_period_sec,
+        scale=scale,
+        min_separation_msec=min_separation_msec,
+        min_match_threshold=min_match_threshold,
+        audio_sample=audio_sample,
+        debug=debug,
+    )
+    if audio_results is None or len(audio_results) == 0:
+        # without audio there is not point in running the video parsing
+        raise Exception(
+            "ERROR: audio calculation failed. Verify that there are signals in audio stream."
+        )
+    # write up the results to disk
+    audio_results.to_csv(outfile, index=False)
+
+
+def media_parse_video(
+    width,
+    height,
+    num_frames,
+    pixel_format,
+    luma_threshold,
+    pre_samples,
+    samplerate,
+    beep_freq,
+    beep_duration_samples,
+    beep_period_sec,
+    scale,
+    infile,
+    outfile,
+    debug,
+    **kwargs,
+):
+    ref_fps = kwargs.get("ref_fps", -1)
+    lock_layout = kwargs.get("lock_layout", False)
+    tag_manual = kwargs.get("tag_manual", False)
+    threaded = kwargs.get("threaded", False)
+    # recalculate the video results
+    video_results = video_parse.video_parse(
+        infile,
+        width,
+        height,
+        pixel_format,
+        ref_fps,
+        luma_threshold,
+        lock_layout=lock_layout,
+        tag_manual=tag_manual,
+        threaded=threaded,
+        debug=debug,
+    )
+    if debug > 0:
+        print(f"Done parsing, write csv, size: {len(video_results)} to {path_video}")
+    # write up the results to disk
+    video_results.to_csv(outfile, index=False)
+
+
+def media_parse(
+    width,
+    height,
+    num_frames,
+    pixel_format,
+    luma_threshold,
+    pre_samples,
+    samplerate,
+    beep_freq,
+    beep_duration_samples,
+    beep_period_sec,
+    scale,
+    infile,
+    outfile,
+    debug,
+    **kwargs,
+):
+    # 1. parse the audio stream
+    path_audio = f"{infile}.audio.csv"
+    media_parse_audio(
+        pre_samples,
+        samplerate,
+        beep_freq,
+        beep_duration_samples,
+        beep_period_sec,
+        scale,
+        infile,
+        path_audio,
+        debug,
+        **kwargs,
+    )
+
+    # 2. parse the video stream
+    path_video = f"{infile}.video.csv"
+    media_parse_video(
+        width,
+        height,
+        num_frames,
+        pixel_format,
+        luma_threshold,
+        pre_samples,
+        samplerate,
+        beep_freq,
+        beep_duration_samples,
+        beep_period_sec,
+        scale,
+        infile,
+        path_video,
+        debug,
+        **kwargs,
+    )
+
+    return path_audio, path_video
--- src/metiq.py
+++ src/metiq.py
@@ -15,6 +15,7 @@ import audio_common
 import audio_generate
 import audio_parse
 import media_generate
+import media_parse
 import vft
 import video_common
 import video_generate
@@ -62,152 +63,6 @@ default_values = {
     "lock_layout": False,
     "audio_sample": "",
 }
-
-
-def media_parse_coverage_video(
-    infile,
-    **kwarg,
-):
-    return video_parse.calc_coverage(
-        infile,
-        width=kwarg.get("width", 0),
-        height=kwarg.get("height", 0),
-        pixel_format=kwarg.get("pixel_format"),
-        debug=kwarg.get("debug", False),
-    )
-
-
-def media_parse_audio(
-    pre_samples,
-    samplerate,
-    beep_freq,
-    beep_duration_samples,
-    beep_period_sec,
-    scale,
-    infile,
-    outfile,
-    debug,
-    **kwargs,
-):
-    min_separation_msec = kwargs.get("min_separation_msec", 50)
-    min_match_threshold = kwargs.get("min_match_threshold", 10)
-    audio_sample = kwargs.get("audio_sample", "")
-
-    audio_results = audio_parse.audio_parse(
-        infile,
-        pre_samples=pre_samples,
-        samplerate=samplerate,
-        beep_freq=beep_freq,
-        beep_duration_samples=beep_duration_samples,
-        beep_period_sec=beep_period_sec,
-        scale=scale,
-        min_separation_msec=min_separation_msec,
-        min_match_threshold=min_match_threshold,
-        audio_sample=audio_sample,
-        debug=debug,
-    )
-    if audio_results is None or len(audio_results) == 0:
-        # without audio there is not point in running the video parsing
-        raise Exception(
-            "ERROR: audio calculation failed. Verify that there are signals in audio stream."
-        )
-    # write up the results to disk
-    audio_results.to_csv(outfile, index=False)
-
-
-def media_parse_video(
-    width,
-    height,
-    num_frames,
-    pixel_format,
-    luma_threshold,
-    pre_samples,
-    samplerate,
-    beep_freq,
-    beep_duration_samples,
-    beep_period_sec,
-    scale,
-    infile,
-    outfile,
-    debug,
-    **kwargs,
-):
-    ref_fps = kwargs.get("ref_fps", -1)
-    lock_layout = kwargs.get("lock_layout", False)
-    tag_manual = kwargs.get("tag_manual", False)
-    threaded = kwargs.get("threaded", False)
-    # recalculate the video results
-    video_results = video_parse.video_parse(
-        infile,
-        width,
-        height,
-        pixel_format,
-        ref_fps,
-        luma_threshold,
-        lock_layout=lock_layout,
-        tag_manual=tag_manual,
-        threaded=threaded,
-        debug=debug,
-    )
-    if debug > 0:
-        print(f"Done parsing, write csv, size: {len(video_results)} to {path_video}")
-    # write up the results to disk
-    video_results.to_csv(outfile, index=False)
-
-
-def media_parse(
-    width,
-    height,
-    num_frames,
-    pixel_format,
-    luma_threshold,
-    pre_samples,
-    samplerate,
-    beep_freq,
-    beep_duration_samples,
-    beep_period_sec,
-    scale,
-    infile,
-    outfile,
-    debug,
-    **kwargs,
-):
-    # 1. parse the audio stream
-    path_audio = f"{infile}.audio.csv"
-    media_parse_audio(
-        pre_samples,
-        samplerate,
-        beep_freq,
-        beep_duration_samples,
-        beep_period_sec,
-        scale,
-        infile,
-        path_audio,
-        debug,
-        **kwargs,
-    )
-
-    # 2. parse the video stream
-    path_video = f"{infile}.video.csv"
-    media_parse_video(
-        width,
-        height,
-        num_frames,
-        pixel_format,
-        luma_threshold,
-        pre_samples,
-        samplerate,
-        beep_freq,
-        beep_duration_samples,
-        beep_period_sec,
-        scale,
-        infile,
-        path_video,
-        debug,
-        **kwargs,
-    )
-
-    return path_audio, path_video
 
 
 # TODO(chema): implement as a function of media_parse
@@ -1268,7 +1123,7 @@ def main(argv):
             )
 
     elif options.func == "parse":
-        media_parse(options)
+        media_parse.media_parse(options)
 
     elif options.func == "analyze":
         media_analyze(options)
@@ -1318,7 +1173,7 @@ def media_analyze(options):
             outfile = options.outfile.split(".csv")[0]
 
         if options.calc_coverage:
-            media_parse_noise_video(
+            media_parse.media_parse_noise_video(
                 infile=infile,
                 outfile=outfile,
                 vft_id=options.vft_id,
