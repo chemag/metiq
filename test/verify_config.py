86e390403e18626a9757ae48a2f59f4d40e391d4
JohanBlome <jblome@fb.com>
2024-04-23 11:06:11 -0700

    metiq: fix for unittest parameters
       * fix for new metiq parameter order
       * added environment params for configuring unittest debug
         1) METIQ_KEEP_FILES
         2) METIQ_DEBUG
    
    Signed-off-by: JohanBlome <jblome@fb.com>

--- test/verify_config.py
+++ test/verify_config.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
+import os
 
-
-KEEP_FILES = False
-DEBUG = 0
+KEEP_FILES = os.environ.get("METIQ_KEEP_FILES", False)
+DEBUG = int(os.environ.get("METIQ_DEBUG", 1))
 PREC = 0.01  # sec
